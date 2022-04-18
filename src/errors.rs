096708ea6294f1409b3a009e784a2570e626ed4d
MTRNord <mtrnord1@gmail.com>
2022-04-18 15:18:08 +0200

    Clearer logs

--- src/errors.rs
+++ src/errors.rs
@@ -1,13 +1,14 @@
 use crate::StateMap;
 use string_cache::{Atom, EmptyStaticAtomSet};
 use thiserror::Error;
+
 #[derive(Error, Debug)]
 pub enum StateCompressorError {
-    #[error("Missing state_goup: {0}")]
+    #[error("Missing state_goup: {}", .0)]
     MissingStateGroup(i64),
-    #[error("Missing prev_state_group: {0}")]
+    #[error("Missing prev_state_group: {}", .0)]
     MissingPrevStateGroup(i64),
-    #[error("States for group {} do not match. Expected {:#?}, found {:#?}",.0, .1, .2)]
+    #[error("States for group {} do not match. Expected {:#?}, found {:#?}", .0, .1, .2)]
     StateMissmatchedForGroup(
         i64,
         Box<StateMap<Atom<EmptyStaticAtomSet>>>,
